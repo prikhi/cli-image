15f2f774b5f30c7a991977f0f2a292406cb58f3f
Pavan Rikhi <pavan.rikhi@gmail.com>
2021-05-24 23:26:26 -0400

    Basic Functionality

--- README.md
+++ README.md
@@ -3,27 +3,37 @@
 [![cli-image Build Status](https://travis-ci.org/prikhi/cli-image.svg?branch=master)](https://travis-ci.org/prikhi/cli-image)
 
 
-Describe your project here.
+Show high-res images in the terminal. Requires `w3mimgdisplay`.
+
+Pass optional arguments for scaling & positioning.
+
+```sh
+stack install
+export PATH="~/.local/bin/:${PATH}"
+cli-image <image-path> [WIDTH [HEIGHT [X [Y]]
+```
+
+Press `Enter` to exit.
 
 
 ## Build
 
 You can build the project with stack:
 
-```
+```sh
 stack build
 ```
 
 For development, you can enable fast builds with file-watching,
 documentation-building, & test-running:
 
-```
+```sh
 stack test --haddock --fast --file-watch --pedantic
 ```
 
 To build & open the documentation, run
 
-```
+```sh
 stack haddock --open cli-image
 ```
 
--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,51 @@
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE ViewPatterns #-}
+{-# LANGUAGE RecordWildCards #-}
 module Main where
 
+import           Control.Monad                  ( unless
+                                                , void
+                                                )
+import           Data.Default                   ( def )
+import           System.Environment             ( getArgs )
+import           System.Exit                    ( exitFailure )
+import           System.IO                      ( hPutStrLn
+                                                , stderr
+                                                )
+import           Text.Read                      ( readMaybe )
+import           W3mImgDisplay                  ( Dimensions(..)
+                                                , Position(..)
+                                                , isW3mImgDisplayAvailable
+                                                , showImage
+                                                )
+
 main :: IO ()
-main = putStrLn "hello world"
+main = do
+  hasExe <- isW3mImgDisplayAvailable
+  unless hasExe $ do
+    hPutStrLn stderr "Could not find w3mimgdisplay in $PATH"
+    exitFailure
+  getArgs >>= \case
+    [imgFile] -> do
+      showImage imgFile def def
+      void getLine
+    [imgFile, readMaybe -> width] -> do
+      showImage imgFile def (def { width = width })
+      void getLine
+    [imgFile, readMaybe -> width, readMaybe -> height] -> do
+      showImage imgFile def Dimensions { .. }
+      void getLine
+    [imgFile, readMaybe -> width, readMaybe -> height, readMaybe -> x] -> do
+      showImage imgFile (def { _x = x }) Dimensions { .. }
+      void getLine
+    [imgFile, readMaybe -> width, readMaybe -> height, readMaybe -> _x, readMaybe -> _y]
+      -> do
+        showImage imgFile Position { .. } Dimensions { .. }
+        void getLine
+
+    args -> do
+      hPutStrLn
+        stderr
+        "Expected up to 5 arguments - image file path, width, height, x, y."
+      hPutStrLn stderr $ "Got: " <> show args
+      exitFailure
